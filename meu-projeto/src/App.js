5e0d3a6bab7b295e3a891cc1d13ec1d1682c9f4f
Caio Prado de Souza <cpssouza7@gmail.com>
2024-04-27 20:54:35 -0300

    update project

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,31 +2,21 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Contato from './pages/Contato'
 import Empresa from './pages/Empresa'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
 
 function App() {
 
 
   return (
     <Router>
-      <ul>
-        <li>
-          <Link exact to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/empresa">Empresa</Link>
-        </li>
-        <li>
-          <Link to="/contato">Contato</Link>
-        </li>
-      </ul>
-
+      <NavBar />
       <Routes>
         <Route exact path='/' element={<Home />}> </Route>
         <Route exact path='/empresa' element={<Empresa />}> </Route>
         <Route exact path='/contato' element={<Contato />}> </Route>
       </Routes>
-
-
+      <Footer />
     </Router>
   )
 }
