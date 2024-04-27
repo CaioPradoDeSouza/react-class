39a29a1a048bb52d3265df2c2ab131f906702761
Caio Prado de Souza <cpssouza7@gmail.com>
2024-04-26 21:46:59 -0300

    update project

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,19 +1,34 @@
-
-import './App.css'
-import Evento from './components/Evento'
-import Form from './components/Form'
-import Condicional from './components/Condicional';
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Empresa from './pages/Empresa'
 
 function App() {
 
+
   return (
-    <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condicional />
-    </div>
-  );
+    <Router>
+      <ul>
+        <li>
+          <Link exact to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/empresa">Empresa</Link>
+        </li>
+        <li>
+          <Link to="/contato">Contato</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route exact path='/' element={<Home />}> </Route>
+        <Route exact path='/empresa' element={<Empresa />}> </Route>
+        <Route exact path='/contato' element={<Contato />}> </Route>
+      </Routes>
+
+
+    </Router>
+  )
 }
 
 export default App
---
+++ meu-projeto/src/components/Saudacao.js
@@ -0,0 +1,18 @@
+function Saudacao({nome}){
+
+    function gerarSaudacao(algunNome){
+        return(
+            `Olá, ${algunNome}, tudo bem?`
+        )
+    }
+
+    return(
+
+    <>
+        {nome && <p>{gerarSaudacao(nome)}</p>}
+    </>
+
+)
+}
+
+export default Saudacao
