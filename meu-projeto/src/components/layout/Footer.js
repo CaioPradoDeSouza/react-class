96efa2e98ed04e2665f37512054a107b55258a4b
Caio Prado de Souza <cpssouza7@gmail.com>
2024-04-29 21:29:13 -0300

    update project

--- meu-projeto/src/components/layout/Footer.js
+++ meu-projeto/src/components/layout/Footer.js
@@ -1,6 +1,18 @@
+import {FaFacebook, FaInstagram, FaLinkedin, FaYoutube} from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
 function Footer(){
     return(
-        <p>Footer</p>
+        <p>
+            <ul className={styles.social_list}>
+                <li className={styles.social_list_li}><FaFacebook /></li>
+                <li className={styles.social_list_li}><FaInstagram /></li>
+                <li className={styles.social_list_li}><FaLinkedin /></li>
+                <li className={styles.social_list_li}><FaYoutube /></li>
+            </ul>
+        </p>
+        
     )
 
 }
